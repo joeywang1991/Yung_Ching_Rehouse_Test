509a404d0b981faaef0bb2228de34bc0a9b3d39f
Joey_Wang <joeywang1991@gmail.com>
2020-10-16 21:37:39 +0800

    加入專案檔案。

---
+++ Test/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Test
+{
+    public class BundleConfig
+    {
+        // 如需統合的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Assets/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Assets/Scripts/jquery.validate*"));
+
+            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
+            // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Assets/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Assets/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Assets/Content/bootstrap.css",
+                      "~/Assets/Content/site.css"));
+        }
+    }
+}
